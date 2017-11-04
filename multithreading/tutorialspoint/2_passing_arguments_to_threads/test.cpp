655b0928f152db9b5356695ed4d699d430a7a478
Unknown <sasha_durham@hotmail.com>
2017-11-04 23:42:16 +0300

    .

--- multithreading/tutorialspoint/2_passing_arguments_to_threads/test.cpp
+++ multithreading/tutorialspoint/2_passing_arguments_to_threads/test.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 struct thread_data {
    int  thread_id;
-   char *message;
+   char* message;
 };
 
 void* PrintHello(void* threadarg) {
@@ -20,10 +20,10 @@ void* PrintHello(void* threadarg) {
 
 int main () {
    pthread_t threads[NUM_THREADS];
-   struct thread_data td[NUM_THREADS];
+   /*ORIG struct*/ thread_data td[NUM_THREADS];
    int rc;
    int i;
-   for( i = 0; i < NUM_THREADS; i++ ) {
+   for (i = 0; i < NUM_THREADS; i++) {
       cout << "main() : creating thread, " << i << endl;
       td[i].thread_id = i;
       td[i].message = "This is message";
