d03a8680bf10cdab9fef8249d857312911f66697
Unknown <sasha_durham@hotmail.com>
2017-11-05 14:20:48 +0300

    .

--- multithreading/tutorialspoint/3_joining_detaching_threads/test.cpp
+++ multithreading/tutorialspoint/3_joining_detaching_threads/test.cpp
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include <unistd.h>
 using namespace std;
+#include <stdio.h>
 
 #define NUM_THREADS 5
 
@@ -11,9 +12,9 @@ void* wait(void* t) {
    int i;
    long tid;
    tid = (long)t;
+   printf("Sleeping in thread %li...\n", tid);
    sleep(1);
-   cout << "Sleeping in thread " << endl;
-   cout << "Thread with id : " << tid << "  ...exiting " << endl;
+   printf("Thread with id %li exiting.\n", tid);
    pthread_exit(NULL);
 }
 
@@ -23,25 +24,26 @@ int main() {
    pthread_t threads[NUM_THREADS];
    pthread_attr_t attr;
    void* status;
-   pthread_attr_init(&attr); // Initialize and set thread joinable
-   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+   pthread_attr_init(&attr); //Initialize thread
+   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); //Set thread joinable
    for (i = 0; i < NUM_THREADS; i++) {
-      cout << "main() : creating thread, " << i << endl;
-      rc = pthread_create(&threads[i], NULL, wait, (void *)i );
+      printf("main(): Creating thread %i.\n", i);
+      rc = pthread_create(&threads[i], NULL, wait, (void*)i);
       if (rc) {
-         cout << "Error:unable to create thread," << rc << endl;
+         cout << "Error: unable to create thread " << rc << endl;
          exit(-1);
       }
    }
    pthread_attr_destroy(&attr); // free attribute and wait for the other threads
    for (i = 0; i < NUM_THREADS; i++) {
+      printf("main(): Joining thread id %i...\n", i); //TEST
       rc = pthread_join(threads[i], &status);
       if (rc) {
-         cout << "Error:unable to join," << rc << endl;
+         cout << "Error: unable to join," << rc << endl;
          exit(-1);
       }
-      cout << "Main: completed thread id :" << i << "  exiting with status :" << status << endl;
+      printf("main(): Joined thread id %i, exiting with status %lu.\n", i, (long)status);
    }
-   cout << "Main: program exiting." << endl;
+   cout << "main(): Program exiting.\n";
    pthread_exit(NULL);
 }
