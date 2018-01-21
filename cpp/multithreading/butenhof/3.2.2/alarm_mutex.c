25966d24532921d1790d957c9f96bca487e0a70d
Unknown <sasha_durham@hotmail.com>
2018-01-21 20:36:35 +0300

    .

--- cpp/multithreading/butenhof/3.2.2/alarm_mutex.c
+++ cpp/multithreading/butenhof/3.2.2/alarm_mutex.c
@@ -18,9 +18,8 @@ void* alarm_thread(void* arg) { //The alarm thread's start routine.
 	time_t now;
 	int status;
 	while (1) { //Loop forever, processing commands. The alarm thread will be disintegrated when the process exits.
-		status = pthread_mutex_lock(&alarm_mutex);
+		status = pthread_mutex_lock(&alarm_mutex); if (status != 0) err_abort(status, "Lock mutex");
 
-		if (status != 0) err_abort(status, "Lock mutex");
 		alarm = alarm_list;
 		if (alarm == NULL) { //If the alarm list is empty, wait for one second. This allows the main thread to run, and read another command. If the list is not empty, remove the first item. Compute the number of seconds to wait — if the result is less than 0 (the time has passed), then set the sleep_time to 0.
 			sleep_time = 1;
@@ -36,13 +35,13 @@ void* alarm_thread(void* arg) { //The alarm thread's start routine.
 			#endif
 		} //42		
 
-		status = pthread_mutex_unlock (&alarm_mutex); /*52-58: The mutex is always unlocked before sleeping or yielding. If the mutex remained locked, then main would be unable to insert a new alarm on the list. That would make the program behave synchronously—the user would have to wait until the alarm expired before doing anything else.*/ /*Unlock the mutex before waiting, so that the main thread can lock it to insert a new alarm request. If the sleep_time is 0, then call sched_yield, giving the main thread a chance to run if it has been readied by user input, without delaying the message if there's no input.*/ if (status != 0) err_abort (status, "Unlock mutex");
+		status = pthread_mutex_unlock(&alarm_mutex); /*52-58: The mutex is always unlocked before sleeping or yielding. If the mutex remained locked, then main would be unable to insert a new alarm on the list. That would make the program behave synchronously—the user would have to wait until the alarm expired before doing anything else.*/ /*Unlock the mutex before waiting, so that the main thread can lock it to insert a new alarm request. If the sleep_time is 0, then call sched_yield, giving the main thread a chance to run if it has been readied by user input, without delaying the message if there's no input.*/ if (status != 0) err_abort (status, "Unlock mutex");
 		if (sleep_time > 0)
 			sleep(sleep_time); //52-58: Calling sleep blocks alarm_thread for the required period of time—it cannot run until the timer expires.
 		else //52-58: Calling sched_yield will yield the processor to a thread that is ready to run, but will return immediately if there are no ready threads. In this case, it means that the main thread will be allowed to process a user command if there's input waiting—but if the user hasn't entered a command, sched_yield will return immediately.
 			sched_yield(); //58
 		if (alarm != NULL) { //If a timer expired, print the message and free the structure.
-			printf ("(%d) %s\n", alarm->seconds, alarm->message);
+			printf("(%d) %s\n", alarm->seconds, alarm->message);
 			free(alarm);
 		}
 	}
@@ -55,7 +54,7 @@ int main(int argc, char *argv[]) { //Each request is queued to a server thread, 
 	pthread_t thread;
 	status = pthread_create(&thread, NULL, alarm_thread, NULL); /*8-11: Create the server thread that will process all alarm requests.*/ if (status != 0) err_abort (status, "Create alarm thread"); /*11*/
 	while (1) {
-		printf("alarm> "); //13-28: Read and process a command, much as in any of the other versions of our alarm program. As in alarm_thread.c, the data is stored in a heap structure allocated by malloc.
+		printf("alarm> \n"); //13-28: Read and process a command, much as in any of the other versions of our alarm program. As in alarm_thread.c, the data is stored in a heap structure allocated by malloc.
 		if (fgets(line, sizeof(line), stdin) == NULL) exit (0);
 		if (strlen (line) <= 1) continue;
 		alarm = (alarm_t*)malloc(sizeof(alarm_t)); if (alarm == NULL) errno_abort("Allocate alarm");
@@ -64,12 +63,11 @@ int main(int argc, char *argv[]) { //Each request is queued to a server thread, 
 			free(alarm); //28
 		} else {
 			status = pthread_mutex_lock(&alarm_mutex); /*30-32: The program needs to add the alarm request to alarm_list, which is shared by both alarm_thread and main. So we start by locking the mutex that synchronizes access to the shared data, alarm_mutex.*/ if (status != 0) err_abort(status, "Lock mutex"); /*32*/
-
 			alarm->time = time(NULL) + alarm->seconds; //33 Because alarm_thread processes queued requests, serially, it has no way of knowing how much time has elapsed between reading the command and processing it. Therefore, the alarm structure includes the absolute time of the alarm expiration, which we calculate by adding the alarm interval, in seconds, to the current number of seconds since the UNIX Epoch, as returned by the time function.
 			last = &alarm_list; //39-49: The alarms are sorted in order of expiration time on the alarm_list queue. //Insert the new alarm into the list of alarms, sorted by expiration time.
-			next = /*ORIG: *last*/ /*TEST:*/alarm_list;
-			while (next != NULL) { //39-49: The insertion code searches the queue
-				if (next->time >= alarm->time) { //39-49: until it finds the first entry with a time greater than or equal to the new alarm's time.
+			next = *last; printf("main 1: Before while (next != NULL). \n"); /*TEST: next = alarm_list;*/ //My test works for sure but only at the first loop. Then, after "*last = alarm;" below, it needs checking but seems correct.
+			while (next != NULL) { printf("main 2: After while (next != NULL). \n"); //39-49: The insertion code searches the queue
+				if (/*ORIG: next->time >= alarm->time*/ alarm->time <= next->time) { //39-49: until it finds the first entry with a time greater than or equal to the new alarm's time.
 					alarm->link = next; //39-49: The new entry is inserted preceding the located entry.
 					*last = alarm; //39-49: Because alarm_list is a simple linked list, the traversal maintains a current entry pointer (this) and a pointer to the previous entry's link member, or to the alarm_list head pointer (last).
 					break;
