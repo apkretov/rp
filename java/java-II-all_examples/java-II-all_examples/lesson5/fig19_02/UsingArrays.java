c4fce2a14cabfc4aa4553539c5a99540b4da7a39
Unknown <sasha_durham@hotmail.com>
2018-03-11 14:06:54 +0300

    .

--- java/java-II-all_examples/java-II-all_examples/lesson5/fig19_02/UsingArrays.java
+++ java/java-II-all_examples/java-II-all_examples/lesson5/fig19_02/UsingArrays.java
@@ -84,18 +84,3 @@ public class UsingArrays
          System.out.println( "8763 not found in intArray" ); 
    } // end main
 } // end class UsingArrays
-
-/**************************************************************************
- * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
