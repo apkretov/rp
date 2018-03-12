cabf3a2d3f14b841eaa425eb4232936d74163812
Unknown <sasha_durham@hotmail.com>
2018-03-12 07:26:20 +0300

    .

--- java/java-II-all_examples/lesson3/fig11_31_32/MouseDetails.java
+++ java/java-II-all_examples/lesson3/fig11_31_32/MouseDetails.java
@@ -1,14 +1,11 @@
-// Fig. 11.32: MouseDetails.java
-// Testing MouseDetailsFrame.
+// Fig. 11.32: MouseDetails.java // Testing MouseDetailsFrame.
 import javax.swing.JFrame;
 
-public class MouseDetails
-{
-   public static void main( String args[] )
-   { 
+public class MouseDetails {
+   public static void main(String args[]) { 
       MouseDetailsFrame mouseDetailsFrame = new MouseDetailsFrame(); 
-      mouseDetailsFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-      mouseDetailsFrame.setSize( 400, 150 ); // set frame size
-      mouseDetailsFrame.setVisible( true ); // display frame
-   } // end main
-} // end class MouseDetails
+      mouseDetailsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      mouseDetailsFrame.setSize(400, 150); // set frame size
+      mouseDetailsFrame.setVisible(true); // display frame
+   }
+}
--- java/java-II-all_examples/lesson3/fig11_31_32/MouseDetailsFrame.java
+++ java/java-II-all_examples/lesson3/fig11_31_32/MouseDetailsFrame.java
@@ -1,5 +1,4 @@
-// Fig. 11.31: MouseDetailsFrame.java
-// Demonstrating mouse clicks and distinguishing between mouse buttons.
+// Fig. 11.31: MouseDetailsFrame.java // Demonstrating mouse clicks and distinguishing between mouse buttons.
 import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
@@ -7,41 +6,29 @@ import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class MouseDetailsFrame extends JFrame 
-{
+public class MouseDetailsFrame extends JFrame {
    private String details; // String representing 
    private JLabel statusBar; // JLabel that appears at bottom of window
 
-   // constructor sets title bar String and register mouse listener
-   public MouseDetailsFrame()
-   {
+   public MouseDetailsFrame() { // constructor sets title bar String and register mouse listener
       super( "Mouse clicks and buttons" );
-
       statusBar = new JLabel( "Click the mouse" );
       add( statusBar, BorderLayout.SOUTH );
       addMouseListener( new MouseClickHandler() ); // add handler
-   } // end MouseDetailsFrame constructor
+   }
 
-   // inner class to handle mouse events
-   private class MouseClickHandler extends MouseAdapter 
-   {
-      // handle mouse-click event and determine which button was pressed
-      public void mouseClicked( MouseEvent event )
-      {
+   private class MouseClickHandler extends MouseAdapter { // inner class to handle mouse events
+      public void mouseClicked( MouseEvent event ) { // handle mouse-click event and determine which button was pressed
          int xPos = event.getX(); // get x-position of mouse
          int yPos = event.getY(); // get y-position of mouse
-
-         details = String.format( "Clicked %d time(s)", 
-            event.getClickCount() );
-      
+         details = String.format( "Clicked %d time(s)", event.getClickCount() );
          if ( event.isMetaDown() ) // right mouse button   
             details += " with right mouse button";
          else if ( event.isAltDown() ) // middle mouse button
             details += " with center mouse button";
          else // left mouse button                       
             details += " with left mouse button";
-
          statusBar.setText( details ); // display message in statusBar
-      } // end method mouseClicked
-   } // end private inner class MouseClickHandler
-} // end class MouseDetailsFrame
+      }
+   }
+}
