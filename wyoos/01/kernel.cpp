045143327ddf8388938aa08d9ca0d31b408cbc53
Unknown <sasha_durham@hotmail.com>
2018-04-16 10:04:14 +0300

    .

--- wyoos/01/kernel.cpp
+++ wyoos/01/kernel.cpp
@@ -1,8 +1,10 @@
 void printf(char* str) {
-	static unsigned short* VideoMemory = (unsigned short*)0xb8000;
-
-	for(int i = 0; str[i] != '\0'; ++i)
-		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+	static unsigned short* shrVideoMemory = (unsigned short*)0xb8000; // The address the video card displays characters from.
+	const unsigned short shrHighMask = 0xFF00; // bit mask to keep high 4 bits (hex for 1111 0000) // 3.8a � Bit flags and bit masks @ http://www.learncpp.com/cpp-tutorial/3-8a-bit-flags-and-bit-masks/
+	for(int i = 0; str[i] != '\0'; ++i) {
+		//ORIG shrVideoMemory[i] = (shrVideoMemory[i] & 0xFF00) | str[i];
+		shrVideoMemory[i] = (shrVideoMemory[i] & shrHighMask) | str[i];
+	}
 }
 
 typedef void (*constructor)();
@@ -13,8 +15,8 @@ extern "C" void callConstructors() {
 		(*i)();
 }
 
-//ORIG extern "C" void kernelMain(const void* multiboot_structure, unsigned int /*multiboot_magic*/) { //We can call the function however we want. We will call it kernelMain.
-extern "C" void kernelMain(const void* multiboot_structure, unsigned int multiboot_magic) { //We can call the function however we want. We will call it kernelMain. //Accept multiboot_structure and multiboot_magic that have been put onto the stack in loader.s (29:21 Write your own Operating System in 1 hour).
-	printf("Hello World! --- http://www.AlgorithMan.de");
-	while(1); //Stay in an infinite loop.
+// ORIG extern "C" void kernelMain(const void* multiboot_structure, unsigned int /* multiboot_magic */) { // We can call the function however we want. We will call it kernelMain.
+extern "C" void kernelMain(const void* multiboot_structure, unsigned int multiboot_magic) { // We can call the function however we want. We will call it kernelMain. // Accept multiboot_structure and multiboot_magic that have been put onto the stack in loader.s (29:21 Write your own Operating System in 1 hour).
+	printf("Hello from MyOS!");
+	while(1); // Stay in an infinite loop.
 }
--- wyoos/01/loader.s
+++ wyoos/01/loader.s
@@ -1,5 +1,5 @@
-.set MAGIC, 0x1badb002			/* 'magic number' lets bootloader find the header */ /* Declare constants for the multiboot header. */ ; See all thes at Bootstrap Assembly @ https://wiki.osdev.org/Bare_Bones#Bootstrap_Assembly
-; ORIG .set FLAGS, (1<<0 | 1<<1)
+.set MAGIC, 0x1badb002			/* 'magic number' lets bootloader find the header */ /* Declare constants for the multiboot header. */  /* See all these at Bootstrap Assembly @ https://wiki.osdev.org/Bare_Bones#Bootstrap_Assembly */
+/* ORIG .set FLAGS, (1<<0 | 1<<1) */
 .set ALIGN,    1<<0             /* align loaded modules on page boundaries */
 .set MEMINFO,  1<<1             /* provide memory map */
 .set FLAGS,    ALIGN | MEMINFO  /* this is the Multiboot 'flag' field */
@@ -17,20 +17,20 @@
 .global loader
 
 
-loader:							; The program entry point.
-    mov $kernel_stack, %esp 	; Set the stack pointer to some pointer to some stack. That pointer is declared at the end of the file.
+loader:							/* The program entry point. */
+    mov $kernel_stack, %esp 	/* Set the stack pointer to some pointer to some stack. That pointer is declared at the end of the file. */
     call callConstructors
-    push %eax					; Store on the stack the pointer to the multiboot structure with the RAM size etc. that the bootloader has retrtieved. 27:38 Write your own Operating System in 1 hour.
-    push %ebx					; Store on the stack the magic number that the bootloader has put to ebx. 27:38 Write your own Operating System in 1 hour.
-    call kernelMain				; The infinite loop in kernelMain is not supposed to end. But to ensure that the computer gets hung another infinite cli/hlt loop is arranged below.
+    push %eax					/* Store on the stack the pointer to the multiboot structure with the RAM size etc. that the bootloader has retrtieved. 27:38 Write your own Operating System in 1 hour. */
+    push %ebx					/* Store on the stack the magic number that the bootloader has put to ebx. 27:38 Write your own Operating System in 1 hour. */
+    call kernelMain				/* The infinite loop in kernelMain is not supposed to end. But to ensure that the computer gets hung another infinite cli/hlt loop is arranged below. */
 
 
-_stop:							; The additional infinite loop.
-    cli							; Disable interrupts ; CLI @ https://en.wikipedia.org/wiki/Interrupt_flag#CLI
-    hlt							; Because the HLT instruction halts until an interrupt occurs, the combination of a CLI followed by a HLT is commonly used to intentionally hang the computer. ; CLI @ https://en.wikipedia.org/wiki/Interrupt_flag#CLI
+_stop:							/* The additional infinite loop. */
+    cli							/* Disable interrupts */ /* CLI @ https://en.wikipedia.org/wiki/Interrupt_flag#CLI */
+    hlt							/* Because the HLT instruction halts until an interrupt occurs, the combination of a CLI followed by a HLT is commonly used to intentionally hang the computer. */ /* CLI @ https://en.wikipedia.org/wiki/Interrupt_flag#CLI */
     jmp _stop
 
 
 .section .bss
-.space 2*1024*1024; # 2 MiB
-kernel_stack:					; The position for the kernel stack.
+.space 2*1024*1024				/* # 2 MiB */
+kernel_stack:					/* The position for the kernel stack. */
