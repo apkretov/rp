05023701bedd064dfe5311440908211f421d5064
Unknown <sasha_durham@hotmail.com>
2018-06-17 15:22:40 +0300

    .

--- cpp/job_tests/teramind/task3.cpp
+++ cpp/job_tests/teramind/task3.cpp
@@ -1,6 +1,4 @@
-/*
 #include<stdio.h>
-//#include <exception>
 
 static unsigned fibonacci(unsigned n) {
 	unsigned fiboNum{};
@@ -9,7 +7,7 @@ static unsigned fibonacci(unsigned n) {
 	else if (n == 2)
 		fiboNum = 1;
 	else
-		fiboNum = fibonacci(n-1) + fibonacci(n-2);
+		fiboNum = fibonacci(n - 1) + fibonacci(n - 2);
 	return fiboNum;
 }
 
@@ -17,14 +15,10 @@ int main() {
 	unsigned n{};
 	unsigned fiboNum{};
 	char chrLine[128];
-
-	//scanf("%u", &n);
 	while (1) {
 		printf("Enter a ordinal interger number (> 0) of a term in the Fibonacci sequence: ");
 		if (fgets(chrLine, sizeof(chrLine), stdin) == NULL)
-			//exit(0);
 			return 0;
-		//if (n > 0) {
 		if (sscanf(chrLine, "%u", &n) < 1)
 			fprintf(stderr, "Enter an integer positive number\n");
 		else if (n > 0) {
@@ -35,4 +29,3 @@ int main() {
 	}
 	return 0;
 }
-*/
