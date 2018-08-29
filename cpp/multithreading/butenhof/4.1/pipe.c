49054750b6628a89c0eb0c0fb398c80ea47c8812
Unknown <sasha_durham@hotmail.com>
2018-08-29 09:56:25 +0300

    .

--- cpp/multithreading/butenhof/4.1/pipe.c
+++ cpp/multithreading/butenhof/4.1/pipe.c
@@ -2,25 +2,25 @@
 #include <pthread.h>	// The program shows the pieces of a simple pipeline program. Each thread in the pipeline increments its input value by 1 and passes it to the next thread. The main program reads a series of "command lines" from stdin. A command line is either a number, which is fed into the beginning of the pipeline, or the character "=," which causes the program to read the next result from the end of the pipeline and print it to stdout.
 #include "errors.h"
 
-struct /*ORIG stage_tag*/ stage_t {		// 9						// 9-17 Each stage of a pipeline is represented by a variable of type stage_t.			//Internal structure describing a "stage" in the pipeline. One for each thread, plus a "result stage" where the final thread can stash the value.
+typedef struct stage_tag {		// 9						// 9-17 Each stage of a pipeline is represented by a variable of type stage_t.			//Internal structure describing a "stage" in the pipeline. One for each thread, plus a "result stage" where the final thread can stash the value.
 	pthread_mutex_t mutex;		// Protect data		// 9-17 stage_t contains a mutex to synchronize access to the stage (???).
 	pthread_cond_t condAvail;	// Data available		// 9-17 The avail condition variable is used to signal a stage that data is ready for it to process,
 	pthread_cond_t condReady;	// Ready for data		// 9-17 and each stage signals its own ready condition variable when it is ready for new data.
 	int dataReady;					// Data present
 	long data;						// Data to process	// 9-17 The data member is the data passed from the previous stage,
 	pthread_t thread;				// Thread for stage	// 9-17 thread is the thread operating this stage,
-	struct /*ORIG stage_tag* */stage_t next;		// Next stage			// 9-17 and next is a pointer to the following stage.
-}/*ORIG stage_t*/;						// 17
+	struct stage_tag* next;		// Next stage			// 9-17 and next is a pointer to the following stage.
+}stage_t;						// 17
 
 typedef struct pipe_tag {	// 23							// 23-29 The pipe_t structure describes a pipeline. It provides pointers to the first and last stage of a pipeline.		// External structure representing the entire pipeline.
 	pthread_mutex_t mutex;	// Mutex to protect pipe
-	struct stage_t* head;				// First stage				// 23-29 The first stage, head, represents the first thread in the pipeline.
-	struct stage_t* tail;				// Final stage				// 23-29 The last stage, tail, is a special stage_t that has no thread—it is a place to store the final result of the pipeline.
+	stage_t* head;				// First stage				// 23-29 The first stage, head, represents the first thread in the pipeline.
+	stage_t* tail;				// Final stage				// 23-29 The last stage, tail, is a special stage_t that has no thread—it is a place to store the final result of the pipeline.
 	int stages;					// Number of stages
 	int active;					// Active data elements
 } pipe_t;						// 29
 
-int pipe_send(struct stage_t* sttStage, long lngData) { // Part 2 shows pipe_send, a utility function used to start data along a pipeline, and also called by each stage to pass data to the next stage.		// Internal function to send a "message" to the specified pipe stage. Threads use this to pass along the modified data item.
+int pipe_send(stage_t* sttStage, long lngData) { // Part 2 shows pipe_send, a utility function used to start data along a pipeline, and also called by each stage to pass data to the next stage.		// Internal function to send a "message" to the specified pipe stage. Threads use this to pass along the modified data item.
 	int intStatus;
 	intStatus = pthread_mutex_lock(&sttStage->mutex); if (intStatus != 0) return intStatus;
 	while (sttStage->dataReady) { // 17-23 It begins by waiting on the specified pipeline stage's ready condition variable until it can accept new data.			// If there's data in the pipe stage, wait for it to be consumed.
@@ -34,8 +34,8 @@ int pipe_send(struct stage_t* sttStage, long lngData) { // Part 2 shows pipe_sen
 }
 
 void* pipe_stage(void* varArg) { // Part 3 shows pipe_stage, the start function for each thread in the pipeline. The thread's argument is a pointer to its stage_t structure.		// The thread start routine for pipe stage threads. Each will wait for a data item passed from the caller or the previous stage, modify the data and pass it along to the next (or final) stage.
-	struct stage_t* sttStage = (struct stage_t*)varArg;
-	struct stage_t* sttNextStage = sttStage->next;
+	stage_t* sttStage = (stage_t*)varArg;
+	stage_t* sttNextStage = sttStage->next;
 	int intStatus;
 	intStatus = pthread_mutex_lock(&sttStage->mutex); if (intStatus != 0) err_abort(intStatus, "Lock pipe stage");
 	while (1) { // 16-27 The thread loops forever, processing data. Because the mutex is locked outside the loop, the thread appears to have the pipeline stage's mutex locked all the time. However, it spends most of its time waiting for new data, on the avail condition variable. Remember that a thread automatically unlocks the mutex associated with a condition variable, while waiting on that condition variable. In reality, therefore, the thread spends most of its time with mutex unlocked.
@@ -49,13 +49,13 @@ void* pipe_stage(void* varArg) { // Part 3 shows pipe_stage, the start function 
 }
 
 int pipe_create(pipe_t* sttPipe, unsigned intStages) { // Part 4 shows pipe_create, the function that creates a pipeline. It can create a pipeline of any number of stages, linking them together in a list.			// External interface to create a pipeline. All the data is initialized and the threads created. They'll wait for data.
-	struct stage_t **sttLink = &sttPipe->head, *sttNewStage, *sttStage;
+	stage_t **sttLink = &sttPipe->head, *sttNewStage, *sttStage;
 	int intStatus;
 	intStatus = pthread_mutex_init(&sttPipe->mutex, NULL); if (intStatus != 0)	err_abort(intStatus, "Init pipe mutex");
 	sttPipe->stages = intStages;
 	sttPipe->active = 0;
 	for (unsigned intPipeIndex = 0; intPipeIndex <= intStages; intPipeIndex++) { 	// 18-34 For each stage, it allocates a new stage_t structure and initializes the members. Notice that one additional "stage" is allocated and initialized to hold the final result of the pipeline.
-		sttNewStage = (struct stage_t*)malloc(sizeof(struct stage_t)); if (sttNewStage == NULL) errno_abort("Allocate stage");
+		sttNewStage = (stage_t*)malloc(sizeof(stage_t)); if (sttNewStage == NULL) errno_abort("Allocate stage");
 		intStatus = pthread_mutex_init(&sttNewStage->mutex, NULL); if (intStatus != 0) err_abort(intStatus, "Init stage mutex");
 		intStatus = pthread_cond_init(&sttNewStage->condAvail, NULL); if (intStatus != 0) err_abort(intStatus, "Init avail condition");
 		intStatus = pthread_cond_init(&sttNewStage->condReady, NULL); if (intStatus != 0) err_abort(intStatus, "Init ready condition");
@@ -63,7 +63,7 @@ int pipe_create(pipe_t* sttPipe, unsigned intStages) { // Part 4 shows pipe_crea
 		*sttLink = sttNewStage;
 		sttLink = &sttNewStage->next;
 	} // 34
-	*sttLink = (struct stage_t*)NULL; // Terminate list  // 36-37 The link member of the final stage is set to NULL to terminate the list, and the pipeline's tail is set to point at the final stage. The tail pointer allows pipe_result to easily find the final product of the pipeline, which is stored into the final stage.
+	*sttLink = (stage_t*)NULL; // Terminate list  // 36-37 The link member of the final stage is set to NULL to terminate the list, and the pipeline's tail is set to point at the final stage. The tail pointer allows pipe_result to easily find the final product of the pipeline, which is stored into the final stage.
 	sttPipe->tail = sttNewStage; // Record the tail // 37
 	for (sttStage = sttPipe->head; sttStage->next != NULL; sttStage = sttStage->next) { // 52-59 After all the stage data is initialized, pipe_create creates a thread for each stage. The extra "final stage" does not get a thread—the termination condition of the for loop is that the current stage's next link is not NULL, which means that it will not process the final stage.			// Create the threads for the pipe stages only after all the data is initialized(including all links). Note that the last stage doesn't get a thread, it's just a receptacle for the final pipeline value. At this point, proper cleanup on an error would take up more space than worthwhile in a "simple example," so instead of cancelling and detaching all the threads already created, plus the synchronization object and memory cleanup done for earlier errors, it will simply abort.
 		intStatus = pthread_create(&sttStage->thread, NULL, pipe_stage, (void*)sttStage); if (intStatus != 0) err_abort(intStatus, "Create pipe stage");
@@ -81,7 +81,7 @@ int pipe_start(pipe_t* sttPipe, long lngValue) { // Part 5 shows pipe_start and 
 }
 
 int pipe_result(pipe_t* sttPipe, long *lngResult) { // 23-47 The pipe_result function first checks whether there is an active item in the pipeline. If not, it returns with a status of 0, after unlocking the pipeline mutex.			// Collect the result of the pipeline. Wait for a result if the pipeline hasn't produced one.
-	struct stage_t* tail = sttPipe->tail;
+	stage_t* tail = sttPipe->tail;
 	//ORIG unused: long value;
 	int empty = 0;
 	int intStatus;
