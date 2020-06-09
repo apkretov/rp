e8d8110d95044f14c14d083336e82c0cf07c4b43
Alexander <sasha_durham@hotmail.com>
2020-06-09 13:26:05 +0300

    Auto synchronization by bash sync.sh

--- sync.sh
+++ sync.sh
@@ -1,7 +1,31 @@
+#! Auto synchronization
+echo "Auto synchronization by bash sync.sh started..."
+
+echo
+echo "git status"
 git status
+
+echo
+echo "git add ."
 git add .
+
+echo
+echo "git status"
 git status
+
+
+echo
+echo "git commit -m 'Auto synchronization by bash sync.sh'"
 git commit -m 'Auto synchronization by bash sync.sh'
+
+echo
+echo "git status"
 git status
+
+echo
+echo "git push"
 git push
+
+echo
+echo "git status"
 git status
