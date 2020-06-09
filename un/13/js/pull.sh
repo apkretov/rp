c7673f22f066abce7a3655e5488114c0c28b471e
Alexander <sasha_durham@hotmail.com>
2020-06-09 14:44:33 +0300

    Pan European table 1.1a metadata

---
+++ un/13/js/pull.sh
@@ -0,0 +1,4 @@
+#!
+git checkout master
+git pull
+git checkout -b paneu/table1.1a
---
+++ un/13/js/push.sh
@@ -0,0 +1,4 @@
+#!
+git add .
+git commit -m 'Pan European table 1.1a metadata'
+git push -u origin paneu/table1.1a
