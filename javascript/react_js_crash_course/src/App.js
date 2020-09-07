a256d8c0041ace6e3f4076552d2f4d426fe80ae9
Alexander <sasha_durham@hotmail.com>
2020-09-07 15:11:23 +0300

    .

--- javascript/react_js_crash_course/src/App.js
+++ javascript/react_js_crash_course/src/App.js
@@ -1,9 +1,11 @@
-import React, {Component} from 'react';
-import Todos from './components/Todos';
+import React, { Component } from 'react'; // React.Component option 1.
+// import React from 'react'; // React.Component option 2.
 
+import Todos from './components/Todos';
 import './App.css';
 
-class App extends Component {
+class App extends Component { // React.Component option 1.
+// class App extends React.Component { // React.Component option 2.
   render() {
     return (
       <div className="App">
